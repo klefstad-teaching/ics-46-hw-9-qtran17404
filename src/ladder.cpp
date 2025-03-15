7c2cd27bf7eaed2863b9b37a0aad689426e4d81f
Quan Tran <tranquan17404@gmail.com>
2025-03-15 01:55:45 +0000

    Doing ladder cpp file. get to generate word

---
+++ src/ladder.cpp
@@ -0,0 +1,79 @@
+#include "ladder.h"
+
+using namespace std;
+
+
+void error(string word1, string word2, string msg)
+{
+    cerr << "Error: " << msg << " (" << word1 << ", " << word2 << ")" << endl;
+}
+
+bool edit_distance_within(const std::string& str1, const std::string& str2, int d)
+{
+    int len1 = str1.size(), len2 = str2.size();
+    if(abs(len1-len2) > 1) return false;
+
+    int i = 0, j = 0, diff = 0;
+
+    while(i < len1 && j < len2)
+    {
+        if(str1[i] != str2[j])
+        {
+            ++diff;
+      
+        if(diff > d) return false;
+        if(len1 > len2) ++i;
+        else if(len1 < len2) ++j;
+        else { ++i; ++j;}
+        }
+        else
+        {
+            ++i; ++j;
+        }
+    }
+    return true;
+}
+
+
+bool is_adjacent(const string& word1, const string& word2)
+{
+    return edit_distance_within(word1, word2, 1);
+}
+
+
+vector<string> generate_word_ladder(const string& begin_word, const string& end_word, const set<string>& word_list)
+{
+    if(begin_word == end_word)
+    {
+        error(begin_word,end_word, "Start and end words gotta be different");
+    }
+
+    queue<vector<string>> ladder_queue;
+    ladder_queue.push({begin_word});
+
+    set<string> visited;
+    visited.insert(begin_word);
+
+    while(!ladder_queue.empty())
+    {
+        vector<string> ladder = ladder_queue.front();
+        ladder_queue.pop();
+        string last_word = ladder.back();
+
+        for(const string& word : word_list)
+        {
+            if(is_adjacent(last_word,word) && !visited.count(word))
+            {
+                vector<string> new_ladder = ladder;
+                new_ladder.push_back(word);
+                if(word == end_word) return new_ladder;
+                ladder_queue.push(new_ladder);
+                visited.insert(word);
+            }
+        }
+    }
+    return {};
+}
+void load_words(set<string> & word_list, const string& file_name);
+void print_word_ladder(const vector<string>& ladder);
+void verify_word_ladder();
