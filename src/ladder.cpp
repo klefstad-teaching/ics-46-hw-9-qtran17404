ea022d8ed261f362ae5f5a4e88ae86bb0cc2edd0
Quan Tran <tranquan17404@gmail.com>
2025-03-15 03:55:14 +0000

    debug load_words and generate word_ladder.

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -98,13 +98,13 @@ void print_word_ladder(const vector<string>& ladder)
     }
 
     cout << "Word ladder found: ";
-    
+
     for(size_t i = 0; i < ladder.size(); ++i)
     {
         cout << ladder[i];
         if(i < ladder.size() -1) cout << " ";
     }
-    cout << endl;
+    cout << " " << endl;
 }
 
 void verify_word_ladder()
