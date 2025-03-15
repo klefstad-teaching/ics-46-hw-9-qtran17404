d7a29bc916bce07ade976cf524f76a9e4c7a9d0f
Quan Tran <tranquan17404@gmail.com>
2025-03-15 03:49:55 +0000

    debug

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -97,9 +97,11 @@ void print_word_ladder(const vector<string>& ladder)
         return;
     }
 
+    cout << "Word ladder found: ";
+    
     for(size_t i = 0; i < ladder.size(); ++i)
     {
-        cout << "Word ladder found: " << ladder[i];
+        cout << ladder[i];
         if(i < ladder.size() -1) cout << " ";
     }
     cout << endl;
